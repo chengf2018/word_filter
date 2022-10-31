c7b7d666fb6f9d42ffa5b45769e1a25682dbaf05
chengf2018 <38105186+chengf2018@users.noreply.github.com>
2022-10-31 16:08:43 +0800

    remove magic number

--- lualib/example.lua
+++ lualib/example.lua
@@ -15,12 +15,12 @@ local skip_word = {
 	" "
 }
 
-word_filter.updateword(1, mask_word)
+word_filter.updateword(word_filter_id, mask_word)
 
-word_filter.updateskipword(1, skip_word)
+word_filter.updateskipword(word_filter_id, skip_word)
 
 --check word
-local is_find, badword = word_filter.check(1, "i am a Bad word! ,a ,m, this is test.")
+local is_find, badword = word_filter.check(word_filter_id, "i am a Bad word! ,a ,m, this is test.")
 print("---check word:")
 print(is_find)
 for k,v in pairs(badword) do
@@ -28,11 +28,11 @@ for k,v in pairs(badword) do
 end
 
 --filter word to new string
-local is_filter, newstr, filter_word = word_filter.filter(1, "i am a Bad word! ,a ,m, this is test.")
+local is_filter, newstr, filter_word = word_filter.filter(word_filter_id, "i am a Bad word! ,a ,m, this is test.")
 print("---filter word:")
 print(is_filter, newstr)
 for k,v in pairs(filter_word) do
 	print(v)
 end
 
-word_filter.freectx(1)
+word_filter.freectx(word_filter_id)
